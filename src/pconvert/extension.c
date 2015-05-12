18afcff2db58a4f238aca057ad2add47ff59f6f8
joamag <joamag@gmail.com>
2015-05-12 18:23:56 +0100

    new python extension

---
+++ src/pconvert/extension.c
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+PyObject *extension_register(PyObject *self, PyObject *args) {
+	Py_RETURN_NONE;
+};
+
+PyObject *extension_unregister(PyObject *self, PyObject *args) {
+    Py_RETURN_NONE;
+};
+
+PyObject *extension_blend_images(PyObject *self, PyObject *args) {
+    char *bottom_path, *top_path, *target_path;
+	struct pcv_image bottom, top;
+
+    if(PyArg_ParseTuple(args, "sss", &bottom_path, &top_path, &target_path) == 0) { return NULL; }
+	
+	read_png(bottom_path, &bottom);
+	read_png(top_path, &top);
+	blend_images(&bottom, &top);
+	write_png(&bottom, target_path);
+	release_image(&top);
+	release_image(&bottom);
+
+    Py_RETURN_NONE;
+};
+
+PyMethodDef pconvert_functions[4] = {
+    {
+        "blend_images",
+        extension_blend_images,
+        METH_VARARGS,
+        NULL
+    },
+    {
+        NULL,
+        NULL,
+        0,
+        NULL
+    }
+};
+
+PyMODINIT_FUNC initpconvert(void) {
+    // allocates space for the module object to hold the
+    // module to be created
+    PyObject *pconvert_module;
+
+    // creates the pconvert extension module with the
+    // functions defined in the previous array
+    pconvert_module = Py_InitModule("pconvert", pconvert_functions);
+    if(pconvert_module == NULL) { return; }
+}
