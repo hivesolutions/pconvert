0d0ac36239733da1ce074738684e606c991d2957
joamag <joamag@gmail.com>
2015-05-13 21:18:26 +0100

    new setup tools

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+setuptools.setup(
+    name = "pconvert",
+    version = "0.1.0",
+    author = "Hive Solutions Lda.",
+    author_email = "development@hive.pt",
+    description = "PNG Converter",
+    license = "Apache License, Version 2.0",
+    keywords = "pconvert converted compositor",
+    url = "http://pconvert.hive.pt",
+    ext_modules = [
+        setuptools.Extension(
+            "pconvert",
+            include_dirs = ["/usr/local/include"],
+            libraries = ["m", "png"],
+            library_dirs = ["/usr/local/lib"],
+            sources = [
+                "src/pconvert/extension.c",
+                "src/pconvert/pconvert.c",
+                "src/pconvert/stdafx.c",
+                "src/pconvert/util.c"
+            ]
+        )
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
+    ]
+)
