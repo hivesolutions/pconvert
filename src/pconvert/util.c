c1c3b184be7410d523146aff632bd4342846dc60
joamag <joamag@gmail.com>
2015-05-13 14:07:01 +0100

    new code

--- src/pconvert/util.c
+++ src/pconvert/util.c
@@ -71,9 +71,9 @@ void blend_disjoint_over(
     float abf = 1.0f * (ab / 255.0f);
     float atf = 1.0f * (at / 255.0f);
 
-    r = (png_byte) (atf + abf < 1.0f ? (rt + rb) * (1.0f - atf) / abf : rt + rb);
-    g = (png_byte) (atf + abf < 1.0f ? (gt + gb) * (1.0f - atf) / abf : gt + gb);
-    b = (png_byte) (atf + abf < 1.0f ? (bt + bb) * (1.0f - atf) / abf : bt + bb);
+    r = (png_byte) (atf + abf < 1.0f ? rt + rb * (1.0f - atf) / abf : rt + rb);
+    g = (png_byte) (atf + abf < 1.0f ? gt + gb * (1.0f - atf) / abf : gt + gb);
+    b = (png_byte) (atf + abf < 1.0f ? bt + bb * (1.0f - atf) / abf : bt + bb);
     a = MAX(0, MIN(255, at + ab));
 
     r = MAX(0, MIN(255, r));
