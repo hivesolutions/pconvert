3fae0fc5720c28ec61d123ca78727c9440d31a4e
joamag <joamag@gmail.com>
2015-06-18 12:27:14 +0100

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "pconvert-python",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "PNG Converter",
