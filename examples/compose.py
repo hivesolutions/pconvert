a63c135d141df01485d5793bbc391e67db40b6a1
joamag <joamag@hive.pt>
2018-06-21 18:09:55 +0100

    new example

---
+++ examples/compose.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import pconvert
+
+pconvert.blend_images(
+    os.path.abspath("../assets/demo/sole.png"),
+    os.path.abspath("../assets/demo/front.png")
+    os.path.abspath("result.png")
+)
