22b4d778ea104857adfc898c62855b636b6e92fc
joamag <joamag@hive.pt>
2018-06-21 18:21:28 +0100

    new code

--- examples/blend_multiple.py
+++ examples/blend_multiple.py
@@ -11,9 +11,7 @@ pconvert.blend_multiple(
         os.path.abspath("../assets/demo/back.png"),
         os.path.abspath("../assets/demo/front.png")
     ),
-    os.path.abspath("result.basic.png"),
-    algorithm = "destination_over",
-    is_inline = True
+    os.path.abspath("result.basic.png")
 )
 
 pconvert.blend_multiple(
@@ -22,7 +20,7 @@ pconvert.blend_multiple(
         os.path.abspath("../assets/demo/back.png"),
         os.path.abspath("../assets/demo/front.png")
     ),
-    os.path.abspath("result.source_over.png"),
+    os.path.abspath("result.destination_over.png"),
     algorithm = "destination_over",
     is_inline = True
 )
