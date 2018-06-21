f1679e2d11697fb8d9f668f78061a25f3de57f8a
joamag <joamag@hive.pt>
2018-06-21 18:15:29 +0100

    new files

---
+++ examples/blend _multiple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import pconvert
+
+pconvert.blend_multiple(
+    (
+        os.path.abspath("../assets/demo/sole.png"),
+        os.path.abspath("../assets/demo/back.png"),
+        os.path.abspath("../assets/demo/front.png")
+    ),
+    os.path.abspath("result.png")
+)
