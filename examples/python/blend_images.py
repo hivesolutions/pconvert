92665844c778aef9233fab2b8f96d3dc0ca8b4c8
joamag <joamag@hive.pt>
2018-06-25 18:34:50 +0100

    small fixes

--- examples/python/blend_images.py
+++ examples/python/blend_images.py
@@ -6,13 +6,13 @@ import os
 import pconvert
 
 pconvert.blend_images(
-    os.path.abspath("../assets/demo/sole.png"),
-    os.path.abspath("../assets/demo/back.png"),
+    os.path.abspath("../../assets/demo/sole.png"),
+    os.path.abspath("../../assets/demo/back.png"),
     os.path.abspath("result.png")
 )
 
 pconvert.blend_images(
     os.path.abspath("result.png"),
-    os.path.abspath("../assets/demo/front.png"),
+    os.path.abspath("../../assets/demo/front.png"),
     os.path.abspath("result.png")
 )
--- examples/python/blend_multiple.py
+++ examples/python/blend_multiple.py
@@ -7,18 +7,18 @@ import pconvert
 
 pconvert.blend_multiple(
     (
-        os.path.abspath("../assets/demo/sole.png"),
-        os.path.abspath("../assets/demo/back.png"),
-        os.path.abspath("../assets/demo/front.png")
+        os.path.abspath("../../assets/demo/sole.png"),
+        os.path.abspath("../../assets/demo/back.png"),
+        os.path.abspath("../../assets/demo/front.png")
     ),
     os.path.abspath("result.basic.png")
 )
 
 pconvert.blend_multiple(
     (
-        os.path.abspath("../assets/demo/sole.png"),
-        os.path.abspath("../assets/demo/back.png"),
-        os.path.abspath("../assets/demo/front.png")
+        os.path.abspath("../../assets/demo/sole.png"),
+        os.path.abspath("../../assets/demo/back.png"),
+        os.path.abspath("../../assets/demo/front.png")
     ),
     os.path.abspath("result.destination_over.png"),
     algorithm = "destination_over",
