499484831f107ba4aab3d404d4d52d0c87323df7
joamag <joamag@hive.pt>
2018-06-26 16:40:14 +0100

    new extreme blend

---
+++ examples/python/blend_extreme.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import pconvert
+
+SIZE = 1000000
+
+for i in range(SIZE):
+    pconvert.blend_multiple(
+        (
+            os.path.abspath("../../assets/demo/sole.png"),
+            os.path.abspath("../../assets/demo/back.png"),
+            os.path.abspath("../../assets/demo/front.png")
+        ),
+        os.path.abspath("result.extreme.png")
+    )
+
+    pconvert.blend_multiple(
+        (
+            os.path.abspath("../../assets/demo/sole.png"),
+            os.path.abspath("../../assets/demo/back.png"),
+            os.path.abspath("../../assets/demo/front.png")
+        ),
+        os.path.abspath("result.extreme_destination_over.png"),
+        algorithm = "destination_over",
+        is_inline = True
+    )
+
+    if i % 100 == 0: print(i)
