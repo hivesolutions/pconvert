efd06d4bf2c5d34ebc709f0c0cbee411600b9d6c
João Magalhães <joamag@hive.pt>
2018-10-25 10:47:49 +0100

    better support for python3 in windows

--- src/pconvert/stdafx.c
+++ src/pconvert/stdafx.c
@@ -1,1 +1,16 @@
 #include "stdafx.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900)
+FILE _iob[3] = {
+    NULL,
+    NULL,
+    NULL
+};
+
+FILE *__iob_func() {
+    _iob[0] = *stdin;
+    _iob[1] = *stdout;
+    _iob[2] = *stderr;
+    return _iob;
+}
+#endif
