403e3c115a97aac49262654d7c61e537ed6c0806
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-10-06 10:26:55 +0100

    Add support for optional 'options' dictionary

--- examples/python/blend_images.py
+++ examples/python/blend_images.py
@@ -11,8 +11,15 @@ pconvert.blend_images(
     os.path.abspath("result.png")
 )
 
+# this example supplies an optional parameter called `options` which pconvert-rust uses
+# despite not being used by this pconvert version, it is backwards compatible
 pconvert.blend_images(
     os.path.abspath("result.png"),
     os.path.abspath("../../assets/demo/front.png"),
-    os.path.abspath("result.png")
+    os.path.abspath("result.png"),
+    options = {
+        "num_threads": 20,
+        "compression": "best",
+        "filter": "nofilter"
+    }
 )
