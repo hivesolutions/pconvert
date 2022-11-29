8fe3d961ff1d6506575cf0f542570ef064f98b99
João Magalhães <joamag@gmail.com>
2022-11-29 07:23:02 +0000

    version: 0.4.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [0.4.3] - 2022-11-29
+
+### Fixed
+
+* Issue with version number
+
 ## [0.4.2] - 2022-11-29
 
 ### Fixed
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "pconvert-python",
-    version = "0.4.2",
+    version = "0.4.3",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "PNG Convert",
