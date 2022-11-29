6ffbefe3e70c0440a7dfcd305e3874569cc9c4c8
João Magalhães <joamag@gmail.com>
2022-11-29 09:13:36 +0000

    version: 0.4.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [0.4.5] - 2022-11-29
+
+### Fixed
+
+* Python distribution using only `sdist`
+
 ## [0.4.4] - 2022-11-29
 
 ### Added
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "pconvert-python",
-    version = "0.4.4",
+    version = "0.4.5",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "PNG Convert",
